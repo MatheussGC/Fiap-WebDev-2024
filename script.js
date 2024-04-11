60c0b470cf1cc3f2c7956d65e3e9c028a3d82074
MatheussGC <161664848+MatheussGC@users.noreply.github.com>
2024-04-10 22:55:23 -0300

    WebDev

---
+++ script.js
@@ -0,0 +1,15 @@
+function clicar(){
+    //Entrada
+    var numero1 = parseFloat(document.getElementById("numero1").value)
+    var numero2 = parseFloat(document.getElementById("numero2").value)
+
+    //Processamento
+    var divisão = numero1 / numero2
+    
+
+
+    //Saída
+   resultado.innerText = `o resultado da divisão é ${divisão}` 
+  
+   
+}
